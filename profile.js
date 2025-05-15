0e21346f1098734ed5a61d0528e6f322771912c6
MartHe <mart.helmoja@tktk.ee>
2025-05-15 20:04:44 +0300

    add cv files

--- profile.js
+++ profile.js
@@ -7,9 +7,9 @@ window.onload = () => {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        nameSpan.textContent = data.name;
+        nameSpan.textContent = `${data.first_name} ${data.last_name}`;
         ageSpan.textContent = data.age;
-        phoneSpan.textContent = data.telephone_number;
+        phoneSpan.textContent = data.phone_number;
     })
     .catch(error => console.error('Error loading profile:', error));
 };
